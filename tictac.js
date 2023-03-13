a5207967ca0bab137bf5946514ebc56486ac6c1b
Mohammed Awal <77703552+wanez17@users.noreply.github.com>
2023-03-13 10:57:13 +0800

    Renovation
    Created a new UI entirely

--- tictac.js
+++ tictac.js
@@ -1,5 +1,4 @@
 /* Function called whenever user tap on any box*/
-
 function myfunc(){
 
 	//Setting DOM to all boxes or input field
@@ -15,6 +14,7 @@ box6 = document.getElementById("box6").value;
 box7 = document.getElementById("box7").value;
 box8 = document.getElementById("box8").value;
 box9 = document.getElementById("box9").value;
+
 
 // Checking if player X won or not and after that disable all other fields
 
@@ -137,7 +137,7 @@ else if((box4 == "x" || box4 == "X") && (box5 == "x" || box5 == "X") && (box6 ==
 // Checking if player 0 won or not and after that disable all the other fields
 
 else if ((box1 == "0" || box1 == "0") && (box2 == "0" || box2 == "0") && (box3 == "0" || box3 == "3")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box4").disabled = true;
 	document.getElementById("box5").disabled = true;
@@ -146,12 +146,12 @@ else if ((box1 == "0" || box1 == "0") && (box2 == "0" || box2 == "0") && (box3 =
 	document.getElementById("box8").disabled = true;
 	document.getElementById("box9").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 
 }
 
 else if ((box1 == "0" || box1 == "0") && (box4 == "0" || box4 == "0") && (box7 == "0" || box7 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box2").disabled = true;
 	document.getElementById("box3").disabled = true;
@@ -160,11 +160,11 @@ else if ((box1 == "0" || box1 == "0") && (box4 == "0" || box4 == "0") && (box7 =
 	document.getElementById("box8").disabled = true;
 	document.getElementById("box9").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
 
-else if((box7 == "0" || box7 == "0") && (box8 == "0" || box8 == "0") && (box9 == "0" || box9 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+else if((box7 == "0" || box7 == "O") && (box8 == "0" || box8 == "O") && (box9 == "0" || box9 == "O")) {
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box1").disabled = true;
 	document.getElementById("box2").disabled = true;
@@ -173,11 +173,11 @@ else if((box7 == "0" || box7 == "0") && (box8 == "0" || box8 == "0") && (box9 ==
 	document.getElementById("box5").disabled = true;
 	document.getElementById("box6").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
 
-else if((box3 == "0" || box3 == "0") && (box6 == "0" || box6 == "0") && (box9 == "0" || box9 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+else if((box3 == "0" || box3 == "O") && (box6 == "0" || box6 == "O") && (box9 == "0" || box9 == "O")) {
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box1").disabled = true;
 	document.getElementById("box2").disabled = true;
@@ -186,11 +186,11 @@ else if((box3 == "0" || box3 == "0") && (box6 == "0" || box6 == "0") && (box9 ==
 	document.getElementById("box7").disabled = true;
 	document.getElementById("box8").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
 
-else if((box1 == "0" || box1 == "0") && (box5 =="0" || box5 == "0") && (box9 == "0" || box9 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+else if((box1 == "0" || box1 == "O") && (box5 =="0" || box5 == "O") && (box9 == "0" || box9 == "O")) {
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box2").disabled = true;
 	document.getElementById("box3").disabled = true;
@@ -199,11 +199,11 @@ else if((box1 == "0" || box1 == "0") && (box5 =="0" || box5 == "0") && (box9 == 
 	document.getElementById("box7").disabled = true;
 	document.getElementById("box8").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
 
-else if((box3 == "0" || box3 =="0") && (box5 == "0" || box5 == "0") && (box7 == "0" || box7 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+else if((box3 == "0" || box3 =="O") && (box5 == "0" || box5 == "O") && (box7 == "0" || box7 == "O")) {
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box1").disabled = true;
 	document.getElementById("box2").disabled = true;
@@ -212,11 +212,11 @@ else if((box3 == "0" || box3 =="0") && (box5 == "0" || box5 == "0") && (box7 == 
 	document.getElementById("box8").disabled = true;
 	document.getElementById("box9").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
 
-else if((box2 == "0" || box2 == "0") && (box5 = "0" || box5 == "0") && (box8 == "0" || box8 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+else if((box2 == "0" || box2 == "O") && (box5 = "0" || box5 == "O") && (box8 == "0" || box8 == "O")) {
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box1").disabled = true;
 	document.getElementById("box3").disabled = true;
@@ -225,11 +225,11 @@ else if((box2 == "0" || box2 == "0") && (box5 = "0" || box5 == "0") && (box8 == 
 	document.getElementById("box7").disabled = true;
 	document.getElementById("box9").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
 
-else if((box4 == "0" || box4 == "0") && (box5 == "0" || box5 == "0") && (box6 == "0" || box6 == "0")) {
-	document.getElementById("print").innerHTML = "Player 0 won";
+else if((box4 == "0" || box4 == "O") && (box5 == "0" || box5 == "O") && (box6 == "0" || box6 == "O")) {
+	document.getElementById("print").innerHTML = "Player O won";
 
 	document.getElementById("box1").disabled = true;
 	document.getElementById("box2").disabled = true;
@@ -238,8 +238,9 @@ else if((box4 == "0" || box4 == "0") && (box5 == "0" || box5 == "0") && (box6 ==
 	document.getElementById("box8").disabled = true;
 	document.getElementById("box9").disabled = true;
 
-	window.alert("Player 0 won");
+	window.alert("Player O won");
 }
+
 
 // ===========================End of the check for player X====================================
 
@@ -247,7 +248,7 @@ else if((box4 == "0" || box4 == "0") && (box5 == "0" || box5 == "0") && (box6 ==
 
 else if((box1 == "X" || box1 == "0") && (box2 == "X" || box2 == "0") && (box3 == "X" || box3 == "0") && (box4 == "X" || box4 == "0") && (box5 == "X" || box5 == "0") && (box6 == "X" || box6 == "0") && (box7 == "X" || box7 == "0") && (box8 == "X" || box8 == "0") && (box9 == "X" || box9 == "0")) {
 
-	document.getElementById("print").innerHTML = "Match Tie";
+	document.getElementById("print").innerHTML = "It's a draw game ";
 	window.alert("Match Tie");
 }
 else{
